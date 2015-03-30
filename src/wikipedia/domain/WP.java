40b3fe27c14486d7ca8e66cf912863b4374ade93
albert.suarez.molgo <albert.suarez.molgo@est.fib.upc.edu>
2015-03-30 14:49:45 +0200

    Algun mini cambio. Luego seguire q no tengo tiempo ahora

--- src/wikipedia/domain/WP.java
+++ src/wikipedia/domain/WP.java
@@ -5,52 +5,71 @@ public class WP
 	// private Graph
 	private CommunityCollection cc;
 	
+	// Pre:  True.
+	// Post: Create a empty WP.
 	public WP()
 	{
-		// Ini graph
 		cc = new CommunityCollection();
 	}
 	
-	//Pre: La categoria existe en Graph
+	// Pre:  Category with title 'title' exists in graph.
+	// Post: Return Graph get category by title.
 	Category getCategory(String title)
 	{
+		// PROVISIONAL
 		Category c = new Category("Provisional");
-		return c;		// Provisional
-		// return Graph get category by title
-		
+		return c;		
 	}
-	//Pre: La page existe en Graph
+	
+	// Pre:  Page with title 'title' exists in graph.
+	// Post: Return Graph get page by title.
 	Page getPage(String title)
 	{
-		Page p = new Page("Provisional");	//
-		return p;	// Provisional
-		// return Graph get page by title
+		// PROVISIONAL
+		Page p = new Page("Provisional");
+		return p;
 	}
-	//Pre: c no existe en Graph
+	
+	// Pre:  Category c is not in the Graph.
+	// Post: Category c is in the Graph.
 	void addCategory(Category c)
 	{
-		// Graph add category
+		
 	}
-	//Pre: p no existe en Graph
+	
+	// Pre:  Page c is not in the Graph.
+	// Post: Page c is in the Graph.
 	void addPage(Page p)
 	{
-		// Graph add page
+		
 	}
-	//Pre: c existe en Graph
+	
+	// Pre:  Category c is in the Graph.
+	// Post: Category c is not in the Graph.
 	void delCategory(Category c)
 	{
 		// Graph del c y tus enlaces
 	}
+	
+	// Pre:  Page c is in the Graph.
+	// Post: Page c is not in the Graph.
 	void delPage(Page p)
 	{
 		// Graph del p y sus enlaces
 	}
+	
+	// Pre:  True.
+	// Post: Return true if category with title 'title' exists in graph,
+	//		 false alternately.
 	boolean catExists(String title)
 	{
 		return true; // Porvisional
 		// return Graph contains cat(title)?
 	}
 
+	// Pre:  True.
+	// Post: Return true if page with title 'title' exists in graph,
+	//		 false alternately.
 	boolean pagExists(String title)
 	{
 		return true; // Porvisional
--- src/wikipedia/utils/Print.java
+++ src/wikipedia/utils/Print.java
@@ -6,24 +6,31 @@ import java.io.*;
 
 public class Print
 {
-    // Print with a newline:
+	// Pre:  True
+	// Post: Print 'obj' with a newline
     public static void print(Object obj)
     {
         System.out.println(obj);
     }
-    // Print a newline by itself:
+    
+    // Pre:  True
+    // Post: Print a newline by itself
     public static void print()
     {
         System.out.println();
     }
-    // Print with no line break:
+    
+    // Pre:  True
+    // Post: Print with no line break
     public static void printnb(Object obj)
     {
         System.out.print(obj);
     }
-    // The new Java SE5 printf() (from C):
+    
+    // Pre:  True
+    // Post: The new Java SE5 printf() (from C)
     public static PrintStream
         printf(String format, Object... args) {
         return System.out.printf(format, args);
     }
-} ///:~
+}
