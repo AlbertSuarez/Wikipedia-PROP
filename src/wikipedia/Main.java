4e1a1476e83c8f9ee1c043ddcf2b89bc020d436d
Sergi Granell <xerpi.g.12@gmail.com>
2015-05-06 20:51:36 +0200

    Click to nodes :D

--- src/wikipedia/Main.java
+++ src/wikipedia/Main.java
@@ -10,9 +10,18 @@ import javax.imageio.ImageIO;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.awt.MouseInfo;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.util.mxCellRenderer;
+
+import javax.swing.*;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.model.mxCell;
 
 /**
  * Main Class
@@ -24,14 +33,33 @@ public class Main
 	* The main method
 	* @param args Arguments of main
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
-		/*OGraph g = GraphIO.readGraphWPformat();
+		OGraph g = GraphIO.readGraphWPformat();
 		mxGraph mxg = g.toMxGraph();
+		mxGraphComponent mxgc = new mxGraphComponent(mxg);
 
 		BufferedImage image = mxCellRenderer.createBufferedImage(mxg, null, 1, Color.WHITE, true, null);
 
-		try {
+		JFrame frame = new JFrame("Graph image");
+		JLabel label = new JLabel(new ImageIcon(image));
+		frame.getContentPane().add(label);
+		frame.pack();
+		frame.setVisible(true);
+
+		label.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				Point p = e.getPoint();
+				Object obj = mxgc.getCellAt((int)p.getX(), (int)p.getY());
+				System.out.println("x: " + p.getX() + "  y: " + p.getY());
+				if (obj != null) {
+					mxCell cell = (mxCell)obj;
+					System.out.println(mxg.getLabel(cell));
+				}
+			}
+		});
+
+		/*try {
 			ImageIO.write(image, "PNG", new File("graph.png"));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -39,7 +67,7 @@ public class Main
 			System.out.println("Done!");
 		}*/
 
-		PresentationController pc = new PresentationController();
-		pc.run();
+		//PresentationController pc = new PresentationController();
+		//pc.run();
 	}
 }
