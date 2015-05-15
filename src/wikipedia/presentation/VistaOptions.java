19dabfee70a7faea8af566bebfa453be1a520d83
victor.perez.martos <victor.perez.martos@est.fib.upc.edu>
2015-05-15 17:59:54 +0200

    vista options initiated

--- src/wikipedia/presentation/VistaOptions.java
+++ src/wikipedia/presentation/VistaOptions.java
@@ -10,11 +10,21 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextPane;
+import javax.swing.border.TitledBorder;
+import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
 
 public class VistaOptions extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Create the frame.
@@ -34,7 +44,7 @@ public class VistaOptions extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Canviar de Graph");
+		JButton btnNewButton = new JButton("Change Graph");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -42,7 +52,103 @@ public class VistaOptions extends JFrame {
 			}
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnNewButton.setBounds(143, 115, 169, 31);
+		btnNewButton.setBounds(12, 12, 169, 31);
 		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(29, 90, 104, 25);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnAddCategory = new JButton("Add Category");
+		btnAddCategory.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(textField.getText() != ""){
+					pc.addCat(textField.getText());
+				}
+			}
+		});
+		btnAddCategory.setBounds(145, 90, 117, 25);
+		contentPane.add(btnAddCategory);
+		
+		JButton btnDelCategory = new JButton("Del category");
+		btnAddCategory.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(textField.getText() != ""){
+					pc.delCat(textField.getText());
+				}
+			}
+		});
+		btnDelCategory.setBounds(274, 90, 117, 25);
+		contentPane.add(btnDelCategory);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(29, 147, 104, 25);
+		contentPane.add(textField_1);
+		
+		JButton btnAddPage = new JButton("Add Page");
+		btnAddCategory.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(textField_1.getText() != ""){
+					pc.addPage(textField_1.getText());
+				}
+			}
+		});
+		btnAddPage.setBounds(145, 144, 117, 25);
+		contentPane.add(btnAddPage);
+		
+		JButton btnAddPage_1 = new JButton("Del Page");
+		btnAddCategory.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(textField_1.getText() != ""){
+					pc.delPage(textField_1.getText());
+				}
+			}
+		});
+		btnAddPage_1.setBounds(274, 144, 117, 25);
+		contentPane.add(btnAddPage_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Newmann-Girvan", "Louvain", "Clique percolation"}));
+		comboBox.setBounds(703, 144, 169, 24);
+		contentPane.add(comboBox);
+		
+		JButton btnCommunityDetection = new JButton("Community Detection");
+		btnCommunityDetection.setFont(new Font("Dialog", Font.BOLD, 12));
+		btnCommunityDetection.setBounds(497, 144, 194, 25);
+		contentPane.add(btnCommunityDetection);
+		
+		JButton btnPrintGraph = new JButton("Print Graph");
+		btnAddCategory.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				pc.printGraph();
+			}
+		});
+		btnPrintGraph.setBounds(497, 90, 194, 25);
+		contentPane.add(btnPrintGraph);
+		
+		JButton btnSaveGraph = new JButton("Save Graph");
+		btnSaveGraph.setBounds(703, 90, 169, 25);
+		contentPane.add(btnSaveGraph);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Wikipedia", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(51, 51, 51)));
+		panel.setBounds(492, 189, 385, 361);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(5, 17, 375, 339);
+		panel.add(scrollPane);
+		
+		JTextPane textPane = new JTextPane();
+		scrollPane.setViewportView(textPane);
+		
 	}
 }
