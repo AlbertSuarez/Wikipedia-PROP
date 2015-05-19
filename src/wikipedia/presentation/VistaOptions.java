51c9461bb39cef19eafa92fcdfc2bf4331987579
mikaop <raul.mikaop.zelda@gmail.com>
2015-05-19 17:28:33 +0200

    fixed DefaultComboBoxModel warning

--- src/wikipedia/presentation/VistaOptions.java
+++ src/wikipedia/presentation/VistaOptions.java
@@ -43,7 +43,7 @@ public class VistaOptions extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton btnNewButton = new JButton("Change Graph");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
@@ -54,12 +54,12 @@ public class VistaOptions extends JFrame {
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnNewButton.setBounds(12, 12, 169, 31);
 		contentPane.add(btnNewButton);
-		
+
 		textField = new JTextField();
 		textField.setBounds(29, 90, 104, 25);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JButton btnAddCategory = new JButton("Add Category");
 		btnAddCategory.addMouseListener(new MouseAdapter() {
 			@Override
@@ -72,7 +72,7 @@ public class VistaOptions extends JFrame {
 		});
 		btnAddCategory.setBounds(145, 90, 117, 25);
 		contentPane.add(btnAddCategory);
-		
+
 		JButton btnDelCategory = new JButton("Del category");
 		btnDelCategory.addMouseListener(new MouseAdapter() {
 			@Override
@@ -85,12 +85,12 @@ public class VistaOptions extends JFrame {
 		});
 		btnDelCategory.setBounds(274, 90, 117, 25);
 		contentPane.add(btnDelCategory);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(29, 147, 104, 25);
 		contentPane.add(textField_1);
-		
+
 		JButton btnAddPage = new JButton("Add Page");
 		btnAddPage.addMouseListener(new MouseAdapter() {
 			@Override
@@ -103,7 +103,7 @@ public class VistaOptions extends JFrame {
 		});
 		btnAddPage.setBounds(145, 144, 117, 25);
 		contentPane.add(btnAddPage);
-		
+
 		JButton btnDelPage = new JButton("Del Page");
 		btnDelPage.addMouseListener(new MouseAdapter() {
 			@Override
@@ -116,17 +116,17 @@ public class VistaOptions extends JFrame {
 		});
 		btnDelPage.setBounds(274, 144, 117, 25);
 		contentPane.add(btnDelPage);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Newmann-Girvan", "Louvain", "Clique percolation"}));
+
+		JComboBox<String> comboBox = new JComboBox<String> ();
+		comboBox.setModel(new DefaultComboBoxModel<String> (new String[] {"Newmann-Girvan", "Louvain", "Clique percolation"}));
 		comboBox.setBounds(703, 144, 169, 24);
 		contentPane.add(comboBox);
-		
+
 		JButton btnCommunityDetection = new JButton("Community Detection");
 		btnCommunityDetection.setFont(new Font("Dialog", Font.BOLD, 12));
 		btnCommunityDetection.setBounds(497, 144, 194, 25);
 		contentPane.add(btnCommunityDetection);
-		
+
 		JButton btnPrintGraph = new JButton("Print Graph");
 		btnPrintGraph.addMouseListener(new MouseAdapter() {
 			@Override
@@ -136,7 +136,7 @@ public class VistaOptions extends JFrame {
 		});
 		btnPrintGraph.setBounds(497, 90, 194, 25);
 		contentPane.add(btnPrintGraph);
-		
+
 		JButton btnSaveGraph = new JButton("Save Graph");
 		btnSaveGraph.addMouseListener(new MouseAdapter() {
 			@Override
@@ -146,21 +146,21 @@ public class VistaOptions extends JFrame {
 		});
 		btnSaveGraph.setBounds(703, 90, 169, 25);
 		contentPane.add(btnSaveGraph);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Wikipedia", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(51, 51, 51)));
 		panel.setBounds(492, 189, 385, 361);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(5, 17, 375, 339);
 		panel.add(scrollPane);
-		
+
 		JTextPane textPane = new JTextPane();
 		textPane.setEditable(false);
 		scrollPane.setViewportView(textPane);
-		
+
 		JButton btnShowGraph = new JButton("Show Graph");
 		btnShowGraph.addMouseListener(new MouseAdapter() {
 			@Override
@@ -170,6 +170,6 @@ public class VistaOptions extends JFrame {
 		});
 		btnShowGraph.setBounds(123, 483, 153, 31);
 		contentPane.add(btnShowGraph);
-		
+
 	}
 }
